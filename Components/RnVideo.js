b2b9855e71046533286132cbae35e10526a24c59
ALexander4295502 <yuanzhengstl@gmail.com>
2017-11-14 23:33:48 -0600

    Revert "no message"
    This reverts commit 966f8dd60b4f1bef89b5e446f39dba23552445e3.

--- Components/RnVideo.js
+++ Components/RnVideo.js
@@ -5,152 +5,12 @@ import {
   TextInput,
   View,
   Dimensions,
-  ScrollView,
   Animated
 } from 'react-native';
 import Video from 'react-native-video';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import SampleVideo from '../Resources/792677592.mp4';
-import ProgressBar from 'react-native-progress/Bar';
 
-const THRESHOLD = 150;
-
-export default class RnVideo extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      error: false,
-      buffering: true,
-      animated: new Animated.Value(0),
-      paused: true,
-      duration: 0,
-    };
-    this.position = {
-      start: null,
-      end: null
-    }
-  }
-
-  handleError = (meta) => {
-    const { error : {code}} = meta;
-    let error = "An error occurred playing this video";
-    switch (code){
-      case -11800:
-        error = "Could not load video from URL";
-        break;
-    }
-    this.setState({
-      error
-    });
-  };
-
-  handleLoadStart = () => {
-    this.triggerBufferAnimation();
-  };
-
-  triggerBufferAnimation = () => {
-    this.loopingAnimation = Animated.loop(
-      Animated.timing(this.state.animated, {
-        toValue: 1,
-        duration: 350,
-      })
-    ).start()
-  };
-
-  handleVideoLayout = (e) => {
-    const {height} = Dimensions.get("window");
-    this.position.start = e.nativeEvent.layout.y + THRESHOLD;
-    this.position.end = e.nativeEvent.layout.y + height + THRESHOLD*2;
-  };
-
-  handleScroll = (e) => {
-    const scrollPosition = e.nativeEvent.contentOffset.y;
-    const paused = this.state.paused;
-    const {start, end} = this.position;
-    if(scrollPosition > start && scrollPosition < end && paused){
-      this.setState({paused: false});
-    } else if((scrollPosition > end || scrollPosition < start) && !paused) {
-      this.setState({paused: true});
-    }
-  };
-
-  handleBuffer = (meta) => {
-    meta.isBuffering && this.triggerBufferAnimation();
-    if(this.loopingAnimation && !meta.isBuffering) {
-      this.loopingAnimation.stopAnimation();
-    }
-    this.setState({
-      buffering: meta.isBuffering
-    })
-  };
-
-  render() {
-    // const windowSize = Dimensions.get("window");
-    const {width} = Dimensions.get("window");
-    const height = width * 0.5625;
-    const {error} = this.state;
-    const {buffering} = this.state;
-    const interpolatedAnimation =
-      this.state.animated.interpolate({
-        inputRange: [0, 1],
-        outputRange: ["0deg", "360deg"]
-      });
-    const rotateStyle = {
-      transform: [
-        {rotate: interpolatedAnimation}
-      ]
-    };
-
-    return (
-        <ScrollView scrollEventThrottle={16} onScroll={this.handleScroll}>
-          <View style={styles.fakeContent}>
-            <Text>{this.state.paused ? "Paused" : "Playing"}</Text>
-          </View>
-          <View style={error ? styles.error : (buffering ? styles.buffering : styles.container)}>
-            <Video
-              source={SampleVideo}
-              resizeMode="contain"
-              style={style}
-              repeat
-              onError={this.handleError}
-              onLoadStart={this.handleLoadStart}
-              onBuffer={this.handleBuffer}
-              paused={this.state.paused}
-              onLayout={this.handleVideoLayout}
-              onLoad={this.handleLoad}
-              onProgress={this.handleProgress}
-              onEnd={this.handleEnd}
-              ref={ref => this.player = ref}
-            />
-            {/*<View>*/}
-              {/*<Text style={styles.header}>Login</Text>*/}
-              {/*<TextInput placeholder="Email" style={styles.input}/>*/}
-              {/*<TextInput secureTextEntry placeholder="Password" style={styles.input}/>*/}
-            {/*</View>*/}
-            {/*<View style={styles.bufferCover}>*/}
-              {/*{*/}
-                {/*buffering &&*/}
-                {/*<Animated.View style={rotateStyle}><Icon name="circle-o-notch" size={30} color="#fff"/></Animated.View>*/}
-              {/*}*/}
-            {/*</View>*/}
-            {/*<View style={{alignSelf: 'center'}}>*/}
-              {/*{*/}
-                {/*error &&*/}
-                {/*<Icon name="exclamation-triangle" size={30} color='red' style={{alignSelf: 'center'}}/>*/}
-              {/*}*/}
-              {/*{*/}
-                {/*error &&*/}
-                {/*<Text style={{color: '#fff'}}>{error}</Text>*/}
-              {/*}*/}
-            {/*</View>*/}
-          </View>
-          <View style={styles.fakeContent}>
-            <Text>{this.state.paused ? "Paused" : "Playing"}</Text>
-          </View>
-        </ScrollView>
-    );
-  }
-}
+import SampleVideo from '../Resources/647597071.mp4';
 
 const styles = StyleSheet.create({
   container: {
@@ -196,11 +56,107 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     marginVertical: 15,
     paddingLeft: 15,
-  },
-  fakeContent: {
-    height: 850,
-    backgroundColor: '#CCC',
-    paddingTop: 250,
-    alignItems: 'center',
   }
 });
+
+export default class RnVideo extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      error: false,
+      buffering: true,
+      animated: new Animated.Value(0)
+    };
+  }
+
+  handleError = (meta) => {
+    const { error : {code}} = meta;
+    let error = "An error occurred playing this video";
+    switch (code){
+      case -11800:
+        error = "Could not load video from URL";
+        break;
+    }
+    this.setState({
+      error
+    });
+  };
+
+  handleLoadStart = () => {
+    this.triggerBufferAnimation();
+  };
+
+  triggerBufferAnimation = () => {
+    this.loopingAnimation = Animated.loop(
+      Animated.timing(this.state.animated, {
+        toValue: 1,
+        duration: 350,
+      })
+    ).start()
+  };
+
+  handleBuffer = (meta) => {
+    meta.isBuffering && this.triggerBufferAnimation();
+    if(this.loopingAnimation && !meta.isBuffering) {
+      this.loopingAnimation.stopAnimation();
+    }
+    this.setState({
+      buffering: meta.isBuffering
+    })
+  };
+
+  render() {
+    const windowSize = Dimensions.get("window");
+    console.log(windowSize.height);
+    // const height = width * 0.5625;
+    const {error} = this.state;
+    const {buffering} = this.state;
+    const interpolatedAnimation =
+      this.state.animated.interpolate({
+        inputRange: [0, 1],
+        outputRange: ["0deg", "360deg"]
+      });
+    const rotateStyle = {
+      transform: [
+        {rotate: interpolatedAnimation}
+      ]
+    };
+
+    return (
+      <View style={styles.container}>
+        <View style={error ? styles.error : (buffering ? styles.buffering : styles.container)}>
+          <Video
+            source={SampleVideo}
+            resizeMode="cover"
+            style={StyleSheet.absoluteFill}
+            repeat
+            onError={this.handleError}
+            onLoadStart={this.handleLoadStart}
+            onBuffer={this.handleBuffer}
+          />
+          <View>
+            <Text style={styles.header}>Login</Text>
+            <TextInput placeholder="Email" style={styles.input}/>
+            <TextInput secureTextEntry placeholder="Password" style={styles.input}/>
+          </View>
+          <View style={styles.bufferCover}>
+            {
+              buffering &&
+              <Animated.View style={rotateStyle}><Icon name="circle-o-notch" size={30} color="#fff"/></Animated.View>
+            }
+          </View>
+          <View style={{alignSelf: 'center'}}>
+            {
+              error &&
+              <Icon name="exclamation-triangle" size={30} color='red' style={{alignSelf: 'center'}}/>
+            }
+            {
+              error &&
+              <Text style={{color: '#fff'}}>{error}</Text>
+            }
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
